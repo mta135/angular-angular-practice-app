5fc8d5ad0d4d750faadd96feb645889993efad34
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-10-28 14:25:58 +0200

    adaugare comoponet send-email

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SendEmailComponent } from './component/send-email/send-email.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SendEmailComponent },
+];
---
+++ src/app/component/send-email/send-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-email',
+  imports: [],
+  templateUrl: './send-email.component.html',
+  styleUrl: './send-email.component.scss'
+})
+export class SendEmailComponent {
+
+}
