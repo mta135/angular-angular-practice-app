ce27d9c69873bdf6105c5a10cff255592ce7c135
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-10-29 09:29:31 +0200

    Adaugare rutare

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { SendEmailComponent } from './component/send-email/send-email.component';
+import { HomeComponent } from './component/home/home.component';
+import { InputComponent } from './component/input/input.component';
 
 export const routes: Routes = [
-    { path: '', component: SendEmailComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'input', component: InputComponent },
+    { path: 'send-email', component: SendEmailComponent },
 ];
--- src/app/component/menu-bar/menu-bar.component.ts
+++ src/app/component/menu-bar/menu-bar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
