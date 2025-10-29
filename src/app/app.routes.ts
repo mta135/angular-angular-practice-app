5eb315dc62c138a1348510b69d8cb91a5c91e501
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-10-29 09:43:11 +0200

    Adagare default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './component/home/home.component';
 import { InputComponent } from './component/input/input.component';
 
 export const routes: Routes = [
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
     { path: 'home', component: HomeComponent },
     { path: 'input', component: InputComponent },
     { path: 'send-email', component: SendEmailComponent },
--- src/app/component/menu-bar/menu-bar.component.ts
+++ src/app/component/menu-bar/menu-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, MatMenuModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
