2707da8bc53e257d6a6caf0fb877f89327a136c8
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-10-31 09:07:21 +0200

    Eliminare clase care nu vor fi folosite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { SendEmailComponent } from './component/send-email/send-email.component';
-import { HomeComponent } from './component/home/home.component';
-import { InputComponent } from './component/input/input.component';
 
 export const routes: Routes = [
 
