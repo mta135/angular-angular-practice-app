de358dd68089de4867176bde20048a5dd1f393ff
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-11-03 14:58:27 +0200

    coloare rindrui. In proces

--- src/app/component/send-email/send-email.component.ts
+++ src/app/component/send-email/send-email.component.ts
@@ -3,36 +3,19 @@ import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { FileUploadService } from '../../services/file-upload.service';
 import { EmailMessageModel } from '../../models/email-message.model';
+import { CommonModule } from '@angular/common';
 
 
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 
 
 @Component({
   selector: 'app-send-email',
   standalone: true,
-  imports: [ButtonModule, TableModule],
+  imports: [ButtonModule, TableModule, CommonModule],
   templateUrl: './send-email.component.html',
   styleUrls: ['./send-email.component.scss']
 })
@@ -44,13 +27,11 @@ export class SendEmailComponent {
   constructor(private fileUploadService: FileUploadService) { }
   emailList: EmailMessageModel[] = [];
 
-  dataSource = ELEMENT_DATA;
   selectedFile: File | null = null;
   loading = false;
 
 
   onFileSelected(event: any): void {
-    debugger
     const file: File = event.target.files[0];
 
     if (file) {
@@ -88,6 +69,20 @@ export class SendEmailComponent {
 
 
   }
+
+
+  public myRowClass(status: string) {
+    debugger
+    if (status.toLowerCase() === 'succes') {
+      return 'greenBkg';
+
+    }
+    else {
+      return 'redBkg';
+    }
+
+  }
+
 }
 
 
