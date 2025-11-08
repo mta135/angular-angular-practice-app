c4719b50d9bc883e40e4b42cce8294a21b41ae82
mta <m.tamazlicaru1959@gmail.com>
2025-11-08 19:43:00 +0200

    stop notification

--- src/app/component/send-email/send-email.component.ts
+++ src/app/component/send-email/send-email.component.ts
@@ -140,7 +140,7 @@ export class SendEmailComponent implements OnInit {
       alert('A apărut o eroare. Verificați consola.');
 
     } finally {
-
+      this.notificationService.stopConnection();
     }
   }
 
