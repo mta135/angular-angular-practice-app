10c50b692197fb9b2e2bb3ac1cf88cd5c6c7f0c8
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-12-22 16:01:42 +0200

    adaugare fisier tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 
 import { MenuBarComponent } from "./component/menu-bar/menu-bar.component";
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuBarComponent],
+  imports: [RouterOutlet, MenuBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  showMenu: boolean = true;
+
+  constructor(private router: Router) {
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: any) => {
+      this.showMenu = event.urlAfterRedirects !== '/currency';
+    });
+
+  }
+
   title = 'angular-practice';
 }
