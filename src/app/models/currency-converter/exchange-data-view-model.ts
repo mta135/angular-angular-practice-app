436e974202518bac2740625ef9fef45ecfea352b
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2026-01-05 11:38:04 +0200

    Adaugare valuta

--- src/app/models/currency-converter/exchange-data-view-model.ts
+++ src/app/models/currency-converter/exchange-data-view-model.ts
@@ -7,10 +7,26 @@ export class ExchangeDataViewMode {
 
     public SelectedProvider: CurrencyProvider | null = null;
 
+
+    public Euro: number;
+    public USD: number;
+
+    public GBP: number;
+
+    public RON: number;
+
+    public UAH: number;
+
     constructor() {
         this.Providers = [];
         this.CurrencyRates = [];
         this.SelectedProvider = null;
+
+        this.Euro = 0;
+        this.USD = 0;
+        this.GBP = 0;
+        this.RON = 0;
+        this.UAH = 0;
 
     }
 
