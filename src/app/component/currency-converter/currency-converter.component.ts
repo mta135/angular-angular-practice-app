62c377673350db99198105fcbd5519bfda501c3c
mta <m.tamazlicaru1959@gmail.com>
2026-01-06 19:57:00 +0200

    Fix

--- src/app/component/currency-converter/currency-converter.component.ts
+++ src/app/component/currency-converter/currency-converter.component.ts
@@ -48,11 +48,12 @@ export class CurrencyConverterComponent implements OnInit {
         this.viewModel.Providers = mapper.GetProviders();
 
         this.viewModel.SelectedProvider = mapper.GetSelectedProvider(ExchangeProvider.Bnm);
-        this.viewModel.CurrencyRates = mapper.GetDashboardRates(ExchangeProvider.Bnm);
+        this.viewModel.CurrencyRates = mapper.GetCurrencyRates(ExchangeProvider.Bnm);
 
         this.viewModel.DashBoardRates = this.mapper?.GetDashboardRates(ExchangeProvider.Bnm);
 
         this.viewModel.LeftSelectedRate = this.mapper?.GetRateByCode(ExchangeProvider.Bnm, CurrencyCode.MDL);
+
         this.viewModel.RightSelectedRate = this.mapper?.GetRateByCode(ExchangeProvider.Bnm, CurrencyCode.EUR);
 
         console.log('Datele au fost încărcate și mapate cu succes.');
