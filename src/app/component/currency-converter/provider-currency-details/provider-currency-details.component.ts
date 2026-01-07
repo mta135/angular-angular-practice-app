eff80c491ee4c014fa9184b850058ab91ec7ca80
mta <m.tamazlicaru1959@gmail.com>
2026-01-07 19:13:13 +0200

    Popularea girdului cu date

--- src/app/component/currency-converter/provider-currency-details/provider-currency-details.component.ts
+++ src/app/component/currency-converter/provider-currency-details/provider-currency-details.component.ts
@@ -5,6 +5,9 @@ import { ActivatedRoute } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular'; // Importă componenta Angular
 import { ColDef, AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { CurrencyDataService } from '../../../services/exchage-data-service';
+import { firstValueFrom } from 'rxjs';
+import { CurrencyServiceDataMapper } from '../../../common/mapper/currency-converter-mapper/currency-data-mapper';
+import { ExchangeDataViewMode } from '../../../models/currency-converter/exchange-data-view-model';
 
 // Înregistrează toate modulele Community
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -23,26 +26,45 @@ export class ProviderCurrencyDetailsComponent {
 
   private currencyService = inject(CurrencyDataService);
 
+  private mapper?: CurrencyServiceDataMapper;
+  public viewModel: ExchangeDataViewMode = new ExchangeDataViewMode();
+
+  public rowData: any[] = [];
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute) {
     this.showGrid = !isPlatformServer(this.platformId);
 
   }
 
 
-  ngOnInit(): void {
-    this.selectedProviderCode = this.route.snapshot.paramMap.get('providerCode');
-    console.log('Providerul selectat este:', this.selectedProviderCode);
+  async GetCurrencyProviderData(): Promise<void> {
+
+    let response = await firstValueFrom(this.currencyService.GetCurrencyProvidersData());
+    if (response) {
+
+      this.mapper = new CurrencyServiceDataMapper(response);
+
+      this.selectedProviderCode = this.route.snapshot.paramMap.get('code');
+      this.rowData = this.mapper.GetCurrencyRates(this.selectedProviderCode ?? "");
+
+    }
+    else {
+      // error handler
+    }
+
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.GetCurrencyProviderData();
   }
 
   columnDefs: ColDef[] = [
-    { field: 'code', headerName: 'Valuta' },
-    { field: 'rate', headerName: 'Curs' }
+    { field: 'Code', headerName: 'Valuta', flex: 1 },
+    { field: 'Name', headerName: 'Denumire', flex: 2 },
+    { field: 'Sell', headerName: 'Vânzare', flex: 1 },
+    { field: 'Buy', headerName: 'Cumpărare', flex: 1 }
   ];
 
   // Datele
-  rowData = [
-    { code: 'USD', rate: 17.85 },
-    { code: 'EUR', rate: 19.40 }
-  ];
 
 }
