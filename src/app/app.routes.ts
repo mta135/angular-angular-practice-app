ff7f88188a0d0f193ba1178503543a462c44fa1f
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2026-02-12 12:18:01 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { CurrencyConverterComponent } from './component/currency-converter/currency-converter-data/currency-converter.component';
 import { ProviderCurrencyDetailsComponent } from './component/currency-converter/provider-currency-details/provider-currency-details.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
 
-    { path: '', redirectTo: '/currency-converter', pathMatch: 'full' },
-    { path: 'currency-converter', component: CurrencyConverterComponent },
+    // { path: '', redirectTo: '/currency-converter', pathMatch: 'full' },
+    // { path: 'currency-converter', component: CurrencyConverterComponent },
 
-    { path: 'currency-details/:code', component: ProviderCurrencyDetailsComponent },
+    // { path: 'currency-details/:code', component: ProviderCurrencyDetailsComponent },
+
+
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
 ];
