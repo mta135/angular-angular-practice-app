0d442f1468ed28e4e119f28ae1e3c2205d9cfaa2
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2026-02-12 14:18:25 +0200

    Adaugarea functii pentru calulari

--- src/app/component/calculator/calculator.component.ts
+++ src/app/component/calculator/calculator.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+
+
+  public calculationResult: string = ''
+
+
+
+  public GetNumber(number: string): void {
+
+    this.calculationResult += number;
+    console.log(number);
+
+  }
+
+
+  public GetOperation(operation: string): string {
+
+    this.calculationResult += operation;
+    return operation;
+
+  }
+
+
+
+  private DoOperation(): void {
+
+  }
 
 }
