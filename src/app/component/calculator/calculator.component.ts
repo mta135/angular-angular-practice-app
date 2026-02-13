cbad2af6e8e8c3506558316faa113982dde4691d
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2026-02-13 14:20:25 +0200

    Adaugare functioanal

--- src/app/component/calculator/calculator.component.ts
+++ src/app/component/calculator/calculator.component.ts
@@ -11,28 +11,72 @@ export class CalculatorComponent {
 
   public calculationResult: string = '0'
 
+  firstOperand: number | null = null;
+  operator: string = "";
+  waitForSecondNumber = false;
+
 
   public GetNumber(number: string): void {
 
     this.ClearCalculationResult();
 
-    this.calculationResult += number;
+    if (this.waitForSecondNumber) {
+
+      this.calculationResult = number;
+      this.waitForSecondNumber = false;
+
+    }
+
+    else {
+      this.calculationResult === '0' ? this.calculationResult = number : this.calculationResult += number;
+    }
+
     console.log(number);
-
   }
 
 
-  public GetOperation(operation: string): string {
+  public GetOperation(operation: string): void {
 
-    this.calculationResult += operation;
-    return operation;
+
+    if (this.firstOperand === null) {
+
+      this.firstOperand = Number(this.calculationResult);
+
+    } else if (this.operator) {
+
+      const result = this.doCalculation(this.operator, Number(this.calculationResult))
+      this.calculationResult = String(result);
+      this.firstOperand = result;
+
+    }
+
+    this.operator = operation;
+    this.waitForSecondNumber = true;
 
   }
 
+  private doCalculation(op: any, secondOp: any) {
+    switch (op) {
+      case '+':
+        return this.firstOperand += secondOp;
 
-  private DoOperation(): void {
+      case '-':
 
+        return this.firstOperand -= secondOp;
+      case '*':
+
+        return this.firstOperand *= secondOp;
+      case '/':
+
+        return this.firstOperand /= secondOp;
+
+      case '=':
+        return secondOp;
+    }
   }
+
+
+
 
 
   public Clear() {
